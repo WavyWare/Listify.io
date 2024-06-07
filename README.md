e12f93ff78e5700da00ce9cd71698cb2a69ff180
WavyWare <jflortech@gmail.com>
2024-06-07 15:49:01 +0200

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Listify
+Great tool to make shop list and share them with your family and friends!
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './*.{html,js}',
+      './dist/**/*.{html,js}',
+      './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('daisyui'),
+  ],
+    daisyui: {
+        themes: ["emerald"],
+    },
+}
+
